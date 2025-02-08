c2ce3e2786312ed994569fcc35a8a393ebac0a00
UsuarioGithub <correo@github.com>
2025-02-07 21:46:10 -0500

    1

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "arquisoft",
+        "USER": "maldo",
+        "PASSWORD": "Tatan2005",
         "HOST": "localhost",
         "PORT": "",
     }
